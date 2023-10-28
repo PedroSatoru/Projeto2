e9ccc39f0516c608edcfee12c9db8eacc1b31a78
PedroSatoru <pedrohsatoru@gmail.com>
2023-10-27 22:36:43 -0300

    Criação da função de apagar cliente

--- biblioteca.c
+++ biblioteca.c
@@ -145,3 +145,53 @@ void NovoCliente() {
     printf("%d",quantidade_clientes);
 }
 
+void ApagaCliente() {
+    ler_clientes();
+    ler_extrato();
+    char cpf[15];
+    int cliente_encontrado = 0;
+
+    printf("Digite o CPF do cliente a ser apagado: ");
+    scanf("%s", cpf);
+
+    for (int i = 0; i < quantidade_clientes; i++) {
+        if (strcmp(lista_clientes[i].cpf, cpf) == 0) {
+            cliente_encontrado = 1;
+
+            // Remova o cliente da lista de clientes
+            for (int j = i; j < quantidade_clientes - 1; j++) {
+                strcpy(lista_clientes[j].nome, lista_clientes[j + 1].nome);
+                strcpy(lista_clientes[j].cpf, lista_clientes[j + 1].cpf);
+                strcpy(lista_clientes[j].tipo_conta, lista_clientes[j + 1].tipo_conta);
+                lista_clientes[j].saldo = lista_clientes[j + 1].saldo;
+                strcpy(lista_clientes[j].senha, lista_clientes[j + 1].senha);
+            }
+            quantidade_clientes--;
+
+            // Remova os registros do extrato associados ao cliente
+            for (int j = 0; j < quantidade_extrato; j++) {
+                if (strcmp(lista_extrato[j].cpf, cpf) == 0) {
+                    for (int k = j; k < quantidade_extrato - 1; k++) {
+                        strcpy(lista_extrato[k].nome, lista_extrato[k + 1].nome);
+                        strcpy(lista_extrato[k].cpf, lista_extrato[k + 1].cpf);
+                        strcpy(lista_extrato[k].tipo_conta, lista_extrato[k + 1].tipo_conta);
+                        strcpy(lista_extrato[k].extrato, lista_extrato[k + 1].extrato);
+                    }
+                    quantidade_extrato--;
+                    j--;  // Revisar a posição após a remoção
+                }
+            }
+
+            printf("Cliente foi excluído\n");
+            salvar_clientes();
+            salva_extrato();
+            break;
+        }
+    }
+
+    if (!cliente_encontrado) {
+        printf("Cliente não encontrado\n");
+    }
+}
+
+
